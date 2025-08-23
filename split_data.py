64aecb98dc2b08f75b7c3b30dd19d868929c1bfa
Jesus Fileto <jesus_filetojr@icloud.com>
2025-08-23 12:57:22 -0500

    init commit (new repo)

--- split_data.py
+++ split_data.py
@@ -1,22 +1,11 @@
-# Install dependencies as needed:
-# pip install kagglehub[pandas-datasets]
-import kagglehub
-from kagglehub import KaggleDatasetAdapter
+import polars as pl
 
-# Set the path to the file you'd like to load
-file_path = "~/Downloads/aug_personal_transactions_with_UserId.csv"
+splits = {'train': 'credit_card_transaction_train.csv', 'test': 'credit_card_transaction_test.csv'}
+train_df = pl.read_csv('hf://datasets/pointe77/credit-card-transaction/' + splits['train'])
+test_df = pl.read_csv('hf://datasets/pointe77/credit-card-transaction/' + splits['test'])
+df = pl.concat([train_df, test_df], ignore_index=True)
 
-# Load the latest version
-df = kagglehub.load_dataset(
-  KaggleDatasetAdapter.PANDAS,
-  "shyakanobledavid/personal-transactions-userid-new-transactions",
-  file_path,
-  # Provide any additional arguments like 
-  # sql_query or pandas_kwargs. See the 
-  # documenation for more information:
-  # https://github.com/Kaggle/kagglehub/blob/main/README.md#kaggledatasetadapterpandas
-)
-for user_id, group in df.groupby('userId'):
-    output_filename = f"user_{user_id}.csv"
-    group.to_csv(output_filename, index=False)
-    print(f"Wrote {output_filename}")
+df.write_csv("credit_card_transaction.csv")
+
+print(df.head())
+print(df.shape)
